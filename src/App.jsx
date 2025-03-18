45863dae33f2960712e619eb3d06b6ec4043e63b
Nikhil kholiya <nikhilkholiya08@gmail.com>
2025-03-18 18:31:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
   const skills = [
     "HTML5", "CSS3/Sass", "JavaScript (ES6+)","TypeScript", "React",
     "Next.Js", "Redux", "Tailwind CSS", "Git","Github",
-    "REST APIs","Node.Js","Express.Js","Database","MongoDB"
+    "REST APIs","Node.Js","Express.Js","Databases","MongoDB"
   ]
   const scrollToSection = (sectionId) => {
     const element = document.getElementById(sectionId)
